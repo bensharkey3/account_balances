d8f199a82687275bce599c2ad7f9991c5bc87c33
Ben Sharkey <bensharkey3@gmail.com>
2022-09-12 13:35:48 +1000

    added loan details

--- lambda_function.py
+++ lambda_function.py
@@ -47,15 +47,45 @@ def lambda_handler(event, context):
     table_summary_data = driver.find_element(by=By.ID, value='current-data')
     summary_data_html = table_summary_data.get_attribute('outerHTML')
     
+    # click transactions link
+    transactions = driver.find_element(by=By.ID, value='cBodyContainer_ctl00_hplFacilityTransactions')
+    transactions.click()
+    
+    # scrape transactions to html string
+    table_transactions = driver.find_element(by=By.ID, value='transaction-list')
+    transactions_html = table_transactions.get_attribute('outerHTML')
+    
+    # click loan details link
+    loan_details = driver.find_element(by=By.ID, value='cBodyContainer_ctl00_hplFacilityLoanDetails')
+    loan_details.click()
+    
+    # scrape loan details to html string
+    loan_details = driver.find_element(by=By.ID, value='loan-control')
+    loan_details_html = loan_details.get_attribute('outerHTML')
+    
+    # click interest link
+    interest = driver.find_element(by=By.ID, value='cBodyContainer_ctl00_hplSMSFInterest')
+    interest.click()
+    
+    # scrape interest to html string
+    table_interest = driver.find_element(by=By.ID, value='interest')
+    interest_html = table_interest.get_attribute('outerHTML')
+
+    
     # write html strings to s3 files
     tz = pytz.timezone('Australia/Melbourne')
     filename = datetime.datetime.now(tz).strftime('%Y-%m-%d--%H-%M-%p') + '.txt'
     market_data_html = market_data_html.encode('utf-8')
     summary_data_html = summary_data_html.encode('utf-8')
+    transactions_html = transactions_html.encode('utf-8')
+    interest_html = interest_html.encode('utf-8')
     
     client = boto3.client('s3')
     client.put_object(Body=market_data_html, Bucket='account-balances-scraper', Key='market-data-html-raw/' + filename)
     client.put_object(Body=summary_data_html, Bucket='account-balances-scraper', Key='summary-data-html-raw/' + filename)
+    client.put_object(Body=transactions_html, Bucket='account-balances-scraper', Key='transactions-html-raw/' + filename)
+    client.put_object(Body=interest_html, Bucket='account-balances-scraper', Key='interest-html-raw/' + filename)
+    client.put_object(Body=loan_details_html, Bucket='account-balances-scraper', Key='loan-details-html-raw/' + filename)
 
     
     driver.quit()
