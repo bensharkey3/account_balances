14b0bae4ab540af9ed474f9ed5aa990231060aae
Ben Sharkey <bensharkey3@gmail.com>
2022-10-05 22:21:01 +1100

    added access keys

--- transform_lambda_function.py
+++ transform_lambda_function.py
@@ -5,11 +5,20 @@ import boto3
 import os
 
 
+# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+s3client = boto3.client('s3')
+
+# s3client = boto3.client(
+#     "s3",
+#     aws_access_key_id=AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+
+
 def lambda_handler(event, context):
     
     bucket_name = 'account-balances-scraper'
-    
-    s3client = boto3.client('s3')
 
     # get all object keys in bucket
     s3resource = boto3.resource('s3')
